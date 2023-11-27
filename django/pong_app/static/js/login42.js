3c5e13ba5d2edec3c355c1495caac7ba10a9ae08
ntraun <Theeledo4@gmail.com>
2023-11-27 16:07:05 +0100

    added login

--- django/pong_app/static/js/login42.js
+++ django/pong_app/static/js/login42.js
@@ -1,43 +1,84 @@
-document.getElementById('loginWith42').addEventListener('click', function() {
-	// Step 1: Redirect the user to the 42 API's authorization URL
-	var clientId = 'YOUR_CLIENT_ID';
-	var redirectUri = encodeURIComponent('YOUR_REDIRECT_URI');
-	var url = 'https://api.intra.42.fr/oauth/authorize?client_id=' + clientId + '&redirect_uri=' + redirectUri + '&response_type=code';
-	window.location.href = url;
-  });
-  
-  // Step 2: Handle the authorization code in the URL
-  var code = new URLSearchParams(window.location.search).get('code');
-  if (code) {
-	// Step 3: Exchange the authorization code for an access token
-	var clientId = 'YOUR_CLIENT_ID';
-	var clientSecret = 'YOUR_CLIENT_SECRET';
-	var redirectUri = encodeURIComponent('YOUR_REDIRECT_URI');
-	var url = 'https://api.intra.42.fr/oauth/token';
-	var data = 'grant_type=authorization_code&client_id=' + clientId + '&client_secret=' + clientSecret + '&code=' + code + '&redirect_uri=' + redirectUri;
-  
-	var xhr = new XMLHttpRequest();
-	xhr.open('POST', url, true);
-	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	xhr.onreadystatechange = function() {
-	  if (this.readyState === 4 && this.status === 200) {
-		var response = JSON.parse(this.responseText);
-		var accessToken = response.access_token;
-  
-		// You now have an access token, which you can use to make API requests.
-		// For example, you can get the user's information:
-		var userUrl = 'https://api.intra.42.fr/v2/me';
-		var userXhr = new XMLHttpRequest();
-		userXhr.open('GET', userUrl, true);
-		userXhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
-		userXhr.onreadystatechange = function() {
-		  if (this.readyState === 4 && this.status === 200) {
-			var user = JSON.parse(this.responseText);
-			// Do something with the user's information, like updating the page.
-		  }
+document.addEventListener('DOMContentLoaded', function () {
+	var isLogged = false;
+	var loginLogout = document.getElementById('Login_Logout');
+	var userName = document.getElementById('userName');
+	var userImage = document.getElementById('userImage');
+
+	var user = JSON.parse(sessionStorage.getItem('user'));
+	if (user) {
+		// User is logged in, set state accordingly
+		isLogged = true;
+		loginLogout.innerHTML = 'Logout';
+		userName.innerHTML = user.login;
+		userImage.src = user.image.link;
+		userImage.style.display = 'block';
+	}
+	else{
+		userName.innerHTML = '';
+		userImage.src = '';
+		userImage.style.display = 'none';
+	}
+
+
+	document.getElementById("Login_Logout").addEventListener("click", function () {
+		if (isLogged) {
+			loginLogout.innerHTML = 'Login with 42';
+			isLogged = false;
+			sessionStorage.removeItem('user');
+			userName.innerHTML = '';
+			userImage.src = '';
+			userImage.style.display = 'none';
+		}
+		else {
+			var clientId = 'u-s4t2ud-7c5080717dbb44d8ad2439acf51e0d576db8aaf6f49ef1866fc422e96ca86dd2';
+			var redirectUri = 'http://localhost:8000/homePage/';
+			var url = 'https://api.intra.42.fr/oauth/authorize?client_id=' + clientId + '&redirect_uri=' + redirectUri + '&response_type=code';
+			window.location.href = url;
+		}
+	});
+
+	var code = new URLSearchParams(window.location.search).get('code');
+	if (code) {
+		var clientId = 'u-s4t2ud-7c5080717dbb44d8ad2439acf51e0d576db8aaf6f49ef1866fc422e96ca86dd2';
+		var clientSecret = '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';
+		var redirectUri = 'http://localhost:8000/homePage/';
+		// var url = 'https://api.intra.42.fr/oauth/token';
+
+		var xhr = new XMLHttpRequest();
+		xhr.open('POST', 'https://api.intra.42.fr/oauth/token', true);
+		xhr.onreadystatechange = function () {
+			if (this.readyState === 4 && this.status === 200) {
+				var response = JSON.parse(this.responseText);
+				var accessToken = response.access_token;
+				var userUrl = 'https://api.intra.42.fr/v2/me';
+				var userXhr = new XMLHttpRequest();
+				userXhr.open('GET', userUrl, true);
+				userXhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+				userXhr.onreadystatechange = function () {
+					if (this.readyState === 4 && this.status === 200) {
+						var user = JSON.parse(this.responseText);
+						console.log(user)
+						sessionStorage.setItem('user', JSON.stringify(user));
+						// show username on top right
+						userName.innerHTML = user.login;
+						// show user image on top right
+						console.log(user.image.link);
+						userImage.src = user.image.link;
+						userImage.style.display = 'block';
+					}
+				};
+				userXhr.send();
+			}
 		};
-		userXhr.send();
-	  }
-	};
-	xhr.send(data);
-  }
+		var data = 'grant_type=authorization_code&client_id=' + clientId + '&client_secret=' + clientSecret + '&code=' + code + '&redirect_uri=' + redirectUri;
+		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		xhr.send(data);
+		// if login success set bool
+		isLogged = true;
+		loginLogout.innerHTML = 'Logout';
+
+	}
+	else {
+		console.log('no code');
+	}
+});
